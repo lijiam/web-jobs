e8a317f210868157568d48aed8219eac9fd7bbac
lijiam <583586309@qq.com>
2020-01-08 19:47:17 +0800

    1

--- serve/src/main/java/com/wuzhi/index/controller/IndexController.java
+++ serve/src/main/java/com/wuzhi/index/controller/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "pages/index";
     }
+
+    @RequestMapping("/back")
+    public String back(){
+        return "pages/back-index";
+    }
 }
